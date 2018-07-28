845c6c3ebc8f2cc0f241a30f2928b9bec314d046
David Z <davidzabran@gmail.com>
2018-07-28 06:34:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,9 +37,9 @@ def won?(board)
     position_3 = board[win_index_3]
     #IS THERE A WIINER?
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      winner = "X" return win_combo
+      winner = "X" && return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        winner = "O" return win_combo
+        winner = "O" && return win_combo
     else
       win = false
     end
